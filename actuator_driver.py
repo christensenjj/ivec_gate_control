d4e3ae7860707d45c1f7a64ee34cfbe932d0bd69
Jacob Christensen <christensenjj@msoe.edu>
2020-01-22 20:19:53 -0600

    First Commit

---
+++ actuator_driver.py
@@ -0,0 +1,9 @@
+"""
+Title: Actuator Driver
+File: actuator_driver.py
+Author: Jacob Christensen <christensenjj@msoe.edu>
+Date: 01/22/2020
+Description:
+	This file will contain the functions required to create the appropriate signal 
+	to drive the linear actuator through the motor controller.
+"""
---
+++ driver_manager.py
@@ -0,0 +1,12 @@
+"""
+Title: Driver Manager
+File: driver_manager.py
+Author: Jacob Christensen <christensenjj@msoe.edu>
+Date: 01/20/2020
+Description:
+    This driver manager will act as the entry point for the entire software
+    control system. This file will control access to system hardware through
+    the rest of the drivers. This file will also setup interrupt service
+    routines for real-time response to activation.
+"""
+
