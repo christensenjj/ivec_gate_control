3ac1f5c75181de6c5f38dcdb37f555e097efdf73
Jacob Christensen <christensenjj@msoe.edu>
2020-01-23 12:20:04 -0600

    Added data collection and modbus driver files, also added gpio function declarations and comments

--- gpio_ref.py
+++ gpio_ref.py
@@ -8,9 +8,34 @@ Description:
 """
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
 
+
+
+"""
+A function for setting all of the GPIO pins to their necessary states.
+:returns: NONE
+"""
 def setup_gpio():
-	
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setwarnings(False)
+
+"""
+A function for retrieving the current IP address, read from the external DIP switch
+
+:returns: A value in the range of 0-255
+"""
+def get_ip_address():
+
+
+"""
+A function for setting the status of the LEDs
+
+:param motion: A boolean value that indicates whether or not the gate is in motion
+:param position: An integer in the range 0-100 indicating the position of the gate.
+:param error: A boolean value that indicates if an error has occured or if service
+	      is needed
+:returns: NONE
+"""
+def set_led_out(motion, position, error):
+
 
