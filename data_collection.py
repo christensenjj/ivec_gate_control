0d2a7859714a35fdeadef8c3a419938c642284bc
Jacob Christensen <christensenjj@msoe.edu>
2020-01-26 11:44:18 -0600

    Added comments and function declarations to the data collection file

---
+++ data_collection.py
@@ -0,0 +1,38 @@
+"""
+Title: Data Collection
+File: data_collection.py
+Author: Jacob Christensen <christensenjj@msoe.edu>
+Date: 01/26/2020
+Description:
+	This file will hold much of the functionality required for gathering and processing data
+	from the peripherals of the microcontroller. This file will also pieces of the data 
+	collected back to the memory for future access.
+"""
+
+
+"""
+This function will setup the appropriate pins for communication with the Analog-to-Digital
+converter.
+
+:returns: NONE
+"""
+def adc_setup():
+
+
+"""
+This file will read the current from the Hall Effect sensor.
+
+:returns: A number representing the current value
+"""
+def get_current():
+
+
+"""
+This file will get the position of the actuator by reading the potentiometer value from
+the ADC.
+
+:returns: A number representing the position of the actuator
+"""
+def get_position():
+
+
