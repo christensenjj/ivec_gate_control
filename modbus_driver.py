7d0bb112e50c548edad52c931cf24bd014faabde
Jacob Christensen <christensenjj@msoe.edu>
2020-01-26 13:53:04 -0600

    Added header comment to Modbus driver

---
+++ modbus_driver.py
@@ -0,0 +1,9 @@
+"""
+Title: Modbus Driver
+File: modbus_driver.py
+Author: Jacob Christensen <christensenjj@msoe.edu>
+Date: 01/26/2020
+Description:
+	This file will setup a TCP Modbus Server that monitors and responds to changes in the
+	Modbus memory structure.
+"""
