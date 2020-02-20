4ce88acde500647f4abd8cb926916b7d6bc2b69e
Jacob Christensen <christensenjj@msoe.edu>
2020-02-19 23:10:01 -0600

    Updated driver manager to have functioning modbus server and calls to gate_action

--- driver_manager.py
+++ driver_manager.py
@@ -5,7 +5,8 @@
 # @details This driver manager will act as the entry point for the entire software control system. This file will control access to system hardware through the rest of the drivers. This file will also setup interrupt service routines for real-time response to activation
 
 
-from twisted.internet.task import LoopingCall, reactor
+from twisted.internet.task import LoopingCall, deferLater
+from twisted.internet import reactor
 from actuator_driver import ActuatorDriver
 from gpio_ref import GPIORef
 from data_collection import DataCollection
@@ -14,24 +15,29 @@ import time
 
 ## Open/Close the gate based on digital input or MODBUS command
 def gate_action():
-    if(GPIO.get_di_states()) :
-        if(GPIO.normal_open) :
-            ACTUATOR.set_position(0)
-	else :
+    if(not GPIO.motion):
+        if(GPIO.get_di_states()) :
+            if(GPIO.normal_open) :
+                ACTUATOR.set_position(0)
+            else :
+                ACTUATOR.set_position(100)
+        elif((time.time() * 1000) - GPIO.di_trigger_time) < 15000) :
             ACTUATOR.set_position(100)
-    else if ((time.time() * 1000) - GPIO.di_trigger_time) < 15000 :
-            ACTUATOR.set_position(100)
-    else :
-        ACTUATOR.set_poistion(MODBUS.read_context(MODBUS.context))
+        else :
+            ACTUATOR.set_position(MODBUS.read_context((MODBUS.context,)))
 
 ## Main execution for control system software
 def main():
     # Setup functions
     GPIO.setup_gpio()
     DC.adc_setup()
-    # loop = LoopingCall(MODBUS.read_context, ctxt=(MODBUS.context,))
     ACTUATOR.set_pwm(70)
     ACTUATOR.set_position(40)
+    loop = LoopingCall(gate_action)
+    # init_position = deferLater(reactor, 0.25, MODBUS.write_update, (MODBUS.context, 0))
+    loop.start(0.25)
+    # reactor.run()
+    MODBUS.run_async_server()
     GPIO.gpio_cleanup()
 
 if __name__ == "__main__":
