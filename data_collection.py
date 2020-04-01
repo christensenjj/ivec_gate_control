8e27ba3f899ffcb28a49ea7ecc51c566dd68f52f
Jacob Christensen <christensenjj@msoe.edu>
2020-04-01 15:47:33 -0500

    added ADC functionality to the data collection driver, commented out read functions due to I/O  errors

--- data_collection.py
+++ data_collection.py
@@ -6,6 +6,8 @@
 
 import logging
 
+import Adafruit_ADS1x15
+
 ## This class is used to manage interactions with the ADCs and sensors
 class DataCollection:
 	## Function to setup the DataCollection object
@@ -17,26 +19,36 @@ class DataCollection:
 		handler.setFormatter(formatter)
 		self.logger.addHandler(handler)
 		self.logger.setLevel(logging.INFO)
+		## ADC objects for reading information from the sensors
+		self.adc1 = Adafruit_ADS1x15.ADS1115()
+		self.adc2 = Adafruit_ADS1x15.ADS1115(address=0x49)
 
-	##This function will setup the appropriate pins for communication with the Analog-to-Digital converter.
-	def adc_setup(self):
-		print("TODO: adc_setup")
+	## Gain to be used when reading from the sensors
+	GAIN = 1
+	## Current sensor channel
+	CURRENT = 1
+	## Postion sensor channel
+	POSITION = 2
+
 
 	##This function will read the current from the Hall Effect sensor
 	# @returns: A number representing the current being drawn by the actuator
 	def get_current(self):
-		print("TODO: get_current")
-		return 120
+		current = 0
+		# current = self.adc1.read_adc(CURRENT, gain=GAIN)
+		return current
 
 	##This function will get the position of the actuator by reading the potentiometer value from the ADC.
 	# @returns A number representing the position of the actuator
 	def get_position(self):
-		print("TODO: get_position")
-		return 0
+		position = 0
+		# POSITION = self.adc1.read_adc(POSITION, gain=GAIN)
+		return position
 
 	##This function will write out given information to a log file for future access
 	# @param info Information string to be written to log file
 	def write_data_log(self, info):
 		self.logger.info(info)
+
 
 
